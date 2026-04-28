047de5ac457327e9a935571a45e345f96b1ba713
sefree <panitan103_54@yahoo.com>
2026-04-28 21:22:04 +0700

    Fix version to 0.1.0

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ setup(
     author="Leandro César",
     author_email="ccleandroc@gmail.com",
     url="https://github.com/leandcesar/themoviedb",
-    version="{{VERSION_PLACEHOLDER}}",
+    version="0.1.0",
     license="MIT",
     python_requires=">=3.8",
     packages=find_packages(include=["themoviedb", "themoviedb.*"]),
